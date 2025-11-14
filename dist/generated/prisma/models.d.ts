36836b43864701403bee4df0dcf59315e8edc3c9
Duncan Moiyo <duncan.moiyo@gmail.com>
2025-11-14 13:56:42 +0300

    Stop tracking file

--- dist/generated/prisma/models.d.ts
+++
@@ -1,6 +0,0 @@
-export type * from './models/User';
-export type * from './models/Caregiver';
-export type * from './models/AdultHomeRepresentative';
-export type * from './models/AdultHome';
-export type * from './models/Role';
-export type * from './commonInputTypes';
